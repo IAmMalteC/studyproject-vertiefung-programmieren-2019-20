a116b8d3c653ac953c42e3f2c13639cf764e4466
s0563114 <s0563114@htw-berlin.de>
2019-10-11 19:06:48 +0200

    refactoring bui-buzz

--- excercise/bui-buzz-mc.py
+++ excercise/bui-buzz-mc.py
@@ -2,11 +2,11 @@ firstValue = 3
 secondValue = 5
 
 for i in range (1,100):
-    if (i % firstValue == 0 and i % secondValue != 0):
-        print "Umwelt"
-    elif (i % secondValue == 0 and i % firstValue != 0):
-        print "Informatik"
-    elif (i % firstValue == 0 and i % secondValue == 0):
+    if (i % firstValue == 0 and i % secondValue == 0):
         print "Umweltinformatik"
+    elif (i % firstValue == 0):
+        print "Umwelt"
+    elif (i % secondValue == 0):
+        print "Informatik"
     else:
         print i

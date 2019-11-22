8fc1fecc58ec0078f948f1039fde06e84da00824
s0563114 <s0563114@htw-berlin.de>
2019-11-22 10:35:30 +0100

    mc refacotring app.py

--- excercise/caesar/app.py
+++ excercise/caesar/app.py
@@ -1,22 +1,25 @@
+#loads modules
+from random import randint
+
 while(True):
     textFromUser = input('Feel free to write your text:')
     output = ''
+    #wir wollen immer eine zufällige Nummer ziehen
+    randomNumber = randint(1, 73)
     for letter in textFromUser:
         #Unicoderaum 0-9 = 48-57, A-Z = 65-90, a-z = 97-122
         if letter == ' ':
             newLetter = letter
         else:
-            x = ord(letter)
-            randomNumber = 3
-            xNew = x + randomNumber
-                if xNew < 57 and xNew > 64:
-                    #Um die Sonderzeichen zwischen 9 und A zu überspringen
-                    xNew+= 7
-                elif xNew < 90 and xNew > 97:
-                    #Um die Sonderzeichen zwischen Z und a zu überspringen
-                    xNew+= 7
-                elif xNew < 122:
-                    #Um die Sonderzeichen nach z zu überspringen und zu 0 zurückzukehren
-                    xNew-= 74
-        output = output + char(xNew)
-    print(output)
+            x = ord(letter) + randomNumber
+            if (x > 57 and x < 64) or (x > 90 and x < 97):
+                #Um die Sonderzeichen zwischen 9 und A und zwischen Z und a zu überspringen
+                x += 7
+            elif x > 122:
+                #Um die Sonderzeichen nach z zu überspringen und zu 0 zurückzukehren
+                x -= 74
+                if (x > 57 and x < 64) or (x > 90 and x < 97):
+                    # Um die Sonderzeichen zwischen 9 und A und zwischen Z und a zu überspringen
+                    x += 7
+        output = output + chr(x)
+    print(output, 'Randomfaktror is ', randomNumber)

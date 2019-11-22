5a6df16ccd0f31223c655a0523fb37b9c338906d
s0563114 <s0563114@htw-berlin.de>
2019-11-22 12:00:43 +0100

    other method for encryption mc

--- excercise/caesar/app.py
+++ excercise/caesar/app.py
@@ -1,25 +1,24 @@
-#loads modules
 from random import randint
+import string
 
 while(True):
     textFromUser = input('Feel free to write your text:')
+    #amount in the list
+    listOfCharacters = string.ascii_letters + string.digits
     output = ''
-    #Encryption with a random number
-    randomNumber = randint(1, 73)
+    #Encryption with a random number (26*2+10 = 62)
+    randomNumber = randint(1, 61)
+
     for letter in textFromUser:
-        #Unicode 0-9 = 48-57, A-Z = 65-90, a-z = 97-122
+        #checks for spaces
         if letter == ' ':
-            x = ord(letter)
+            x = letter
         else:
-            x = ord(letter) + randomNumber
-            if (x > 57 and x < 64) or (x > 90 and x < 97):
-                #Um die Sonderzeichen zwischen 9 und A und zwischen Z und a zu überspringen
-                x += 7
-            elif x > 122:
-                #Um die Sonderzeichen nach z zu überspringen und zu 0 zurückzukehren
-                x -= 74
-                if (x > 57 and x < 64) or (x > 90 and x < 97):
-                    # Um die Sonderzeichen zwischen 9 und A und zwischen Z und a zu überspringen
-                    x += 7
-        output = output + chr(x)
-    print(output, 'Encrypted with the factor ', randomNumber)
+            #number =
+            if listOfCharacters.index(letter) + randomNumber < 62:
+                x = listOfCharacters[listOfCharacters.index(letter) + randomNumber]
+            else:
+                x = listOfCharacters[listOfCharacters.index(letter) + randomNumber - 62]
+        output = output + x
+
+    print(output, '\nEncrypted with the factor', randomNumber)

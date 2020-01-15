cc30095acd3bc4292cb8dbf0d1b87fb7d5d0b991
s0563114 <s0563114@htw-berlin.de>
2020-01-16 00:40:49 +0100

    show and dont show offset on html

--- excercise/enryptionTool/static/script.js
+++ excercise/enryptionTool/static/script.js
@@ -15,31 +15,18 @@ function changeOffsetVisibility() {
  * If status contains a number it sows the offset Factor box
  */
 function checkIfCesarWasUsedForEncryption() {
-    var status = getMetaContent(offsetStatus);
-    if (status == ''){
-        document.getElementById("offsetResult").style.display = 'none';
-    }else{
-        document.getElementById("offsetResult").style.display = 'block';
+    var x = document.getElementsByTagName("META");
+    var status = "";
+    var i;
+    for (i = 0; i < x.length; i++) {
+      if (x[i].name == "offsetStatus") {
+        status = x[i].content;
+      }
     }
-}
-/**
- * Get Meta Tag Content
- *
- * @param {string} metaName The meta tag name.
- * @return {string} The meta tag content value, or empty string if not found.
- */
-function getMetaContent(metaName) {
-    var metas = document.getElementsByTagName('meta');
-    var re = new RegExp('\\b' + metaName + '\\b', 'i');
-    var i = 0;
-    var mLength = metas.length;
- 
-    for (i; i < mLength; i++) {
-        if (re.test(metas[i].getAttribute('name'))) {
-            return metas[i].getAttribute('content');
-        }
+    if (status == "") {
+      document.getElementById("offsetResult").style.display = 'none';
+    } else {
+      document.getElementById("offsetResult").style.display = "block";
     }
- 
-    return '';
-}
+  }
 

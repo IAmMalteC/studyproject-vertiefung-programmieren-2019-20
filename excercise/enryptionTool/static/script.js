efa0835b241ed77204b7f070e1f241f6af64004d
s0563114 <s0563114@htw-berlin.de>
2020-02-06 10:16:30 +0100

    tried to fix show-hide offsetbox on reload. Didn't work

--- excercise/enryptionTool/static/script.js
+++ excercise/enryptionTool/static/script.js
@@ -1,23 +1,23 @@
-/**
- * Change the visibility on f.ex. encryption page.
- * If Cesar is choosen it shows the offset box else not.
- */
 function changeOffsetVisibility() {
-    // checks if cesar is checked
+    /**
+     * Change the visibility on f.ex. encryption page.
+     * If Cesar is choosen it shows the offset box else not.
+     * Doesn't work if page is reloaded after mono is selected
+     */
     if (document.getElementById("encryptiontype-0").checked){
         document.getElementById("offset").style.display = 'block';
     } else {
         document.getElementById("offset").style.display = 'none';
     }
 }
-/**
- * Check Status of Encryption Type f. ex. Result page
- * If status contains a number it sows the offset Factor box
- */
+
 function checkIfCesarWasUsedForEncryption() {
+    /**
+     * Check Status of Encryption Type f. ex. Result page
+     * If status contains a number it sows the offset Factor box
+     */
     var meta_elements = document.getElementsByTagName("META");
     var status = "";
-//    var i;
     for (var i = 0; i < meta_elements.length; i++) {
       if (meta_elements[i].name == "offsetStatus") {
         status = meta_elements[i].content;
